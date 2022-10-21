6dc8361b07b105a7383f26cd8b638ecaa0eeecb3
YXHLHM <102135195+YXHLHM@users.noreply.github.com>
2022-10-21 20:35:59 +0800

    Add files via upload

---
+++ minicode-5/index/index.js
@@ -0,0 +1,15 @@
+Component({
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 0
+        })
+      }
+    }
+  },
+ 
+
+
+})
---
+++ minicode-5/index/index2.js
@@ -0,0 +1,14 @@
+Component({
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 2
+        })
+      }
+    }
+  },
+ 
+
+})
